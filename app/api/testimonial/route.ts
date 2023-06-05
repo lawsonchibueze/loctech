624407e8517ae04b8b11679e44e4bdffbc65c556
isaacthajunior <isaacjunez669@gmail.com>
2023-06-06 00:27:55 +0100

    created testimonial route

--- app/api/testimonial/route.ts
+++ app/api/testimonial/route.ts
@@ -1,1 +1,54 @@
 import prisma from "@/prisma/prisma";
+
+export const GET = async () => {
+  const testimonials = await prisma.testimonial.findMany();
+
+  return new Response(JSON.stringify(testimonials), { status: 200 });
+};
+
+export const POST = async (req: Request) => {
+  const { name, image, review, createdAt, updatedAt } = await req.json();
+
+  const newTestimonial = await prisma.testimonial.create({
+    data: {
+      name,
+      image,
+      review,
+      createdAt,
+      updatedAt,
+    },
+  });
+
+  return new Response(JSON.stringify(newTestimonial), { status: 201 });
+};
+
+export const PATCH = async (req: Request) => {
+  const { id, name, image, review, createdAt, updatedAt } = await req.json();
+
+  const updatedTestimonial = await prisma.testimonial.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      image,
+      review,
+      createdAt,
+      updatedAt,
+    },
+  });
+
+  return new Response(JSON.stringify(updatedTestimonial), { status: 200 });
+};
+
+export const DELETE = async (req: Request) => {
+  const { id } = await req.json();
+
+  const deletedTestimonial = await prisma.testimonial.delete({
+    where: {
+      id,
+    },
+  });
+
+  return new Response(JSON.stringify(deletedTestimonial), { status: 200 });
+};
